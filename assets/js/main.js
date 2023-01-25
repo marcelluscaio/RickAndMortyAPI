ff45d7d25563642706dad7da84c4eae70c0187ca
Caio Cabral <ocaiocabral@gmail.com>
2023-01-25 18:58:39 -0400

    feat: add images inside cards on screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,36 @@
-let dados;
+const cardContainer = document.querySelector("#card-container");
+let img ='';
+
 
 fetch('https://rickandmortyapi.com/api/character')
-   .then(respostaDaPromessa1 => respostaDaPromessa1.json())
-   .then(respostaDaPromessa2 => dados = respostaDaPromessa2)
+   .then(resposta => resposta.json())
+   .then(resposta => {
+      resposta.results.forEach(result => {
+            img = result.image;
+            let card = document.createElement('div');
+            card.classList.add("card");
+            card.style.width = "18rem";
+            card.innerHTML =
+             `
+               <img src=${img} class="card-img-top" alt="...">
+               <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               </div>
+            `;
+            cardContainer.appendChild(card);
+
+
+      });
+   })
+
+
+//name, status, species, gender, origin, location, image
+
+
+
+
+
+
 
 //mostrar async we await
 //há diferença entre json() e JSON.stringify x JSON.parse()?
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 - [X] Acessar API Rick adn Morty (https://rickandmortyapi.com/)
-- [ ] Apresentar os dados na tela com paginação
+- [ ] Apresentar os personagens na tela
+- [ ]  Fazer paginação
 - [ ] FAzer a paginacao
 - [ ] Permitir filtro por nome, status, espécie, gênero, origin and location
+- https://getbootstrap.com/docs/5.3/components/navbar/#nav
+- https://rickandmortyapi.com/documentation/
