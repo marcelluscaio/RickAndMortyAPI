82a240a989c055a501ccc6843fdb781168739581
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-27 00:47:10 -0300

    feat(home): align and add bold on text

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,12 +40,12 @@ const createCards = (img, name, status, species, gender, originName, locationNam
    `
       <img src=${img} class="card-img-top" alt="...">
       <div class="card-body">
-         <h2 class="card-title">${name}</h2>
-         <p class="card-text">${status}</p>
-         <p class="card-text">${species}</p>
-         <p class="card-text">${gender}</p>
-         <p class="card-text">${originName}</p>
-         <p class="card-text">${locationName}</p>
+         <h2 class="card-title text-center py-2">${name}</h2>
+         <p class="card-text"><span class="fw-bold">Species: </span>${species}</p>
+         <p class="card-text"><span class="fw-bold">Gender: </span>${gender}</p>
+         <p class="card-text"><span class="fw-bold">Origin: </span>${originName}</p>
+         <p class="card-text"><span class="fw-bold">Current Location: </span>${locationName}</p>
+         <p class="card-text text-center">${status}</p>
       </div>
    `;
    cardContainer.appendChild(card);
