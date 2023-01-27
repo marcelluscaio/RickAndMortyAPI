540ffec154673cb9ff01095611a88bd765aa975f
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-27 00:32:14 -0300

    refactor(home): break down function responsability

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,87 @@
+// Get HTML Elements
 const searchField = document.querySelector("#search-field");
 const searchButton = document.querySelector("#search-button");
 const cardContainer = document.querySelector("#card-container");
 
+
+// Create Empty Variables to Receive Results
+var img = '';
+var name = '';
+var status = '';
+var species = '';
+var gender = '';
+var originName = '';
+var locationName = '';
+
+
+// Add Event Listener for the Search
 searchButton.addEventListener('click', (e) => {
       e.preventDefault();
       const searchTerm = searchField.value;
-      searchCharacter(searchTerm);
+      makeRequest(searchTerm);
    }
 );
 
-const makeRequest = url => {
-   fetch(url)
-      .then(response => response.json())
-      .then(response => renderResponse(response))
-};
 
-const renderResponse = response => {
-   cardContainer.innerHTML = ''; //Inner html ou outro?
-   response.results.forEach(result =>
-      {
-         const img = result.image;
-         const name = result.name;
-         const status = result.status;
-         const species = result.species;
-         const gender = result.gender;
-         const originName = result.origin.name;
-         const locationName = result.location.name;
-         const card = document.createElement('div');
-         card.classList.add("card");
-         card.style.width = "18rem";
-         card.innerHTML =
-         `
-            <img src=${img} class="card-img-top" alt="...">
-            <div class="card-body">
-               <h2 class="card-title">${name}</h2>
-               <p class="card-text">${status}</p>
-               <p class="card-text">${species}</p>
-               <p class="card-text">${gender}</p>
-               <p class="card-text">${originName}</p>
-               <p class="card-text">${locationName}</p>
-            </div>
-         `;
-         cardContainer.appendChild(card);
-      });
-};
-
-const searchCharacter = (searchTerm) => {
+// Get Object Based on the Searched Term
+const makeRequest = (searchTerm) => {
    fetch(`https://rickandmortyapi.com/api/character/?name=${searchTerm}`)
       .then(response => response.json())
       .then(response => renderResponse(response))
+};
+
+
+// Create Card Element
+const createCards = (img, name, status, species, gender, originName, locationName) => {
+   const card = document.createElement('div');
+   card.classList.add("card");
+   card.style.width = "18rem";
+   card.innerHTML =
+   `
+      <img src=${img} class="card-img-top" alt="...">
+      <div class="card-body">
+         <h2 class="card-title">${name}</h2>
+         <p class="card-text">${status}</p>
+         <p class="card-text">${species}</p>
+         <p class="card-text">${gender}</p>
+         <p class="card-text">${originName}</p>
+         <p class="card-text">${locationName}</p>
+      </div>
+   `;
+   cardContainer.appendChild(card);
 }
 
-makeRequest('https://rickandmortyapi.com/api/character');
 
+// Set Variables Values, Transfer values to Existing Cards and Show the Search Result on the Screen
+const showSearchResult = (result, img, name, status, species, gender, originName, locationName) => {
+   img = result.image;
+   name = result.name;
+   status = result.status;
+   species = result.species;
+   gender = result.gender;
+   originName = result.origin.name;
+   locationName = result.location.name;
 
-//'https://rickandmortyapi.com/api/character'
+   createCards(img, name, status, species, gender, originName, locationName);
+} 
+
+// Clear Displayed Result for Every New Search
+
+const clearSearch = () => {
+   cardContainer.replaceChildren(); 
+}
+  
+
+// Loop through object to Find the Result and Display It
+const renderResponse = response => {
+   clearSearch();
+   // const searchResult = response.result;
+   response.results.forEach(result =>
+      {
+         showSearchResult(result);
+        
+      });
+};
 
 
 //Queremos pegar: name, status, species, gender, origin, location, image
