45d93a373c85ed1ec45f4ed25751523c3ed58e99
Carol A. Nikolic <carolnikolic@gmail.com>
2023-02-02 00:45:22 -0300

    feat(home): add dropdown for filter categories

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,25 +1,51 @@
 const filterButton = document.querySelector("#filter-button");
 const filtersList = document.querySelector(".all-filters");
+const categories = document.querySelectorAll(".categories");
+const categoryStatus = document.querySelector("#category-status");
+const categoryGender = document.querySelector("#category-gender");
+const categorySpecies = document.querySelector("#category-species");
 
 function showDropdown(dropdown) {
 	
     if(dropdown.style.display == 'none') {
     dropdown.style.display = 'block';
-    console.log(dropdown.style.display);
 
     } else if(dropdown.style.display == 'block') {
     dropdown.style.display = 'none';
-    console.log(dropdown.style.display);
 
     } else {
         dropdown.style.display = 'block';
     }	
 }
 
+
 filterButton.addEventListener('click', (e) => {
     e.preventDefault();
     showDropdown(filtersList);
 });
+
+
+categories.forEach(category => {
+    category.addEventListener('click', (e) => {
+        
+        e.preventDefault();
+        
+        if(category.innerHTML === "Status"){
+            showDropdown(categoryStatus);
+        } else if(category.innerHTML === "Gender") {
+            showDropdown(categoryGender);
+        } else if(category.innerHTML === "Species") {
+            showDropdown(categorySpecies);
+        } else {
+            showDropdown(categoryStatus);
+            showDropdown(categoryGender);
+            showDropdown(categorySpecies);
+        }
+    });
+});
+   
+
+
 
 
 
