95d85a46f47b42aad8549c97eb605d8ea0bb1f83
Carol A. Nikolic <carolnikolic@gmail.com>
2023-02-01 23:52:08 -0300

    feat(home): add dropdown on filter button

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,12 +1,25 @@
 const filterButton = document.querySelector("#filter-button");
-const modal = document.querySelector("#modal");
+const filtersList = document.querySelector(".all-filters");
+
+function showDropdown(dropdown) {
+	
+    if(dropdown.style.display == 'none') {
+    dropdown.style.display = 'block';
+    console.log(dropdown.style.display);
+
+    } else if(dropdown.style.display == 'block') {
+    dropdown.style.display = 'none';
+    console.log(dropdown.style.display);
+
+    } else {
+        dropdown.style.display = 'block';
+    }	
+}
+
+filterButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    showDropdown(filtersList);
+});
 
 
-filterButton.addEventListener('click', (e) => e.preventDefault());
 
-
-
-
-/* modal.addEventListener('shown.bs.modal', () => {
- filterButton.focus()
-}) */
